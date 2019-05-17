732cd10b007a4d22cc993bc2036cc4dc7387bd51
roman-jordan <rjordan1352@gmail.com>
2019-05-17 09:06:51 -0700

    Up and Running With Smurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,35 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const START_SMURFING = 'START_SMURFING';
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_ERROR = 'FETCH_SUCCESS';
+
+export const addSmurf = smurf => dispatch =>{
+  axios
+    .get()
+    .then()
+    .catch()
+}
+
+export const getSmurfs = smurf => dispatch =>{
+  axios
+    .get('http://localhost:3333')
+    .then(res =>console.log(res.data))
+    .catch(err=>console.log(err))
+}
+
+export const updateSmurf = smurf => dispatch =>{
+  axios
+    .get()
+    .then()
+    .catch()
+}
+
+export const deleteSmurf = smurf => dispatch =>{
+  axios
+    .get()
+    .then()
+    .catch()
+}
+
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,46 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+import {
+    START_SMURFING,
+    FETCHING_SMURFS, 
+    FETCH_SUCCESS,
+    FETCH_ERROR} 
+from '../actions'
+ 
+ const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
  }
-*/
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+ const reducer = (state = initialState, action) =>{
+   switch(action.type){
+
+    case START_SMURFING:{
+      return {
+        smurfs:state.smurfs
+      }
+    }
+
+    case FETCHING_SMURFS:{
+      return {
+        fetchingSmurfs:!state.fetchingSmurfs
+      }
+    }
+
+    case FETCH_SUCCESS:{
+      return {
+        fetchingSmurfs:!state.fetchingSmurfs,
+        smurfs:action.payload
+      }
+    }
+
+    case FETCH_ERROR:{
+      return {
+        error:action.payload
+      }
+    }
+   }
+ }
